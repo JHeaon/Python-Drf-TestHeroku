30423923d6c23f0be55b14473a2ea0b37bcb91a7
JHeaon <j3heawon@gmail.com>
2022-08-22 02:36:55 +0900

    22_08_22_UPDATE

--- books/settings.py
+++ books/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_EKY = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_EKY = os.environ['secret_key']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
